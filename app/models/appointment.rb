56429bca41615e46dbec388c63bc11af493a94b4
Ivy Vetor <ivyvetor@gmail.com>
2016-05-05 17:00:14 -0400

    add reviewed seed file

--- app/models/appointment.rb
+++ app/models/appointment.rb
@@ -6,29 +6,30 @@ class Appointment < ActiveRecord::Base
 
   validates :start_time, :end_time, :mentor_id, :phase, :body, presence: true
 
-  validate :length_of_appointment_is_appropriate?
-  validate :appointment_is_in_the_future?
+  # validate :length_of_appointment_is_appropriate?
+  # validate :appointment_is_in_the_future?
+  # validate :overlap?
 
-  def length_of_appointment_is_appropriate?
-    if (self.end_time - self.start_time)/60 != 30 || (self.end_time - self.start_time)/60 != 60
-      self.errors.add(:end_time, "Appointments must be in 30 or 60 minute increments")
-    end
-  end
+  # def length_of_appointment_is_appropriate?
+  #   if !((self.end_time - self.start_time)/60 == 30) && !((self.end_time - self.start_time)/60 == 60)
+  #     self.errors.add(:end_time, "Appointments must be in 30 or 60 minute increments")
+  #   end
+  # end
 
-  def appointment_is_in_the_future?
-    if self.start_time < Time.now
-      self.errors.add(:start_time, "Appointments must be in the future")
-    end
-  end
+  # def appointment_is_in_the_future?
+  #   if !(self.start_time > Time.now)
+  #     self.errors.add(:start_time, "Appointments must be in the future")
+  #   end
+  # end
 
-  def overlap?
-    any_overlap = Appointment.where(mentor_id: self.mentor_id).any? do |appt|
-      self.start_time < appt.end_time && self.end_time > appt.start_time
-    end
+  # def overlap?
+  #   any_overlap = Appointment.where(mentor_id: self.mentor_id).any? do |appt|
+  #     self.start_time < appt.end_time && self.end_time > appt.start_time
+  #   end
 
-    if any_overlap
-      self.errors.add(start_time:, "Appointments overlap")
-    end
-  end
+  #   if any_overlap
+  #     self.errors.add(:start_time, "Appointments overlap")
+  #   end
+  # end
 
 end
