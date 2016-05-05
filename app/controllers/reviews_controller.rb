376beefb153cb7318997e49f6fee70cfb7e80ee5
David Lange <dlange8181@gmail.com>
2016-05-05 17:13:54 -0400

    add strong params to review controller

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -7,8 +7,10 @@ class ReviewsController < ApplicationController
   end
 
   def create
-    @review = Review.new(review_params)
     @appointment = Appointment.find_by(id: params[:id])
+    @user = User.find_by(id: session[:user_id])
+    @review = Review.new(review_params)
+
     if @review.save
       redirect_to @review
     else
@@ -21,5 +23,10 @@ class ReviewsController < ApplicationController
     @appointment = Appointment.find(params[:id])
     redirect_to @appointment
   end
+
+  private
+    def review_params
+      params.require(:review).permit(:rating, :body)
+    end
 
 end
